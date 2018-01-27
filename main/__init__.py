830bb3fe30b301d3897e8f11ca75b83ab3ad694f
mihodihasan <mihodihasan@gmail.com>
2018-01-28 01:13:13 +0600

    idk

--- main/__init__.py
+++ main/__init__.py
@@ -1,50 +1,67 @@
 import pandas as pd
+import numpy as np
 from sodapy import Socrata as repo
 
 # Example authenticated client (needed for non-public datasets):
 client = repo("data.cincinnati-oh.gov", "t1J7xReA9Slki6fndZ3ocYROj", "mihodihasan@gmail.com", "%&!Mhl0Mb%&!")
 
-results = client.get("w2ka-rfbi", limit=70000)
+results = client.get("w2ka-rfbi", limit=10)
 
 results_df = pd.DataFrame.from_records(results)
-
+data_size = len(results_df)
 # print(results_df.asset.drop_duplicates('first'))
 # print(results_df.longitude.size)
 idx = 0
-date = {}
-day = {}
-for i in results_df['loadts']:
-    date[idx] = i.split('T')[0].split('-')
+date = np.zeros(data_size)
+year = np.zeros(data_size)
+month = np.zeros(data_size)
+day = np.zeros(data_size)
+hour = np.zeros(data_size)
+minute = np.zeros(data_size)
+second = np.zeros(data_size)
+for i in results_df['time']:
+    year[idx] = i.split('T')[0].split('-')[0]
+    month[idx] = i.split('T')[0].split('-')[1]
+    date[idx] = i.split('T')[0].split('-')[2]
+    hour[idx] = i.split('T')[1].split('.')[0].split(':')[0]
+    minute[idx] = i.split('T')[1].split('.')[0].split(':')[1]
+    second[idx] = i.split('T')[1].split('.')[0].split(':')[2]
+    # print(i.split('T')[1].split('.')[0].split(':'))
     # print (date)
     import datetime
 
-    day_no = datetime.datetime(int(date[idx][0]), int(date[idx][1]), int(date[idx][2])).weekday()
-    if day_no == 0:
-        day[idx] = 'Monday'
-    elif day_no == 1:
-        day[idx] = 'Tuesday'
-    elif day_no == 2:
-        day[idx] = 'Wednesday'
-    elif day_no == 3:
-        day[idx] = 'Thursday'
-    elif day_no == 4:
-        day[idx] = 'Friday'
-    elif day_no == 5:
-        day[idx] = 'Saturday'
-    elif day_no == 6:
-        day[idx] = 'Sunday'
+    day[idx] = datetime.datetime(int(year[idx]), int(month[idx]), int(date[idx])).weekday()
 
-    # datesArray=s.split('T')[0].split('-')
-    # print(datesArray)
     idx = idx + 1
-day_series = pd.Series(day)
+# day_series = pd.Series(day)
+# day_series.name = 'Day'
+# date_series = pd.Series(date.tolist())
+# date_series.name = 'Date'
+# time_series = pd.Series(time.tolist())
+# time_series.name = 'Time'
 
-target = pd.DataFrame.from_items(
-    [(results_df.latitude.name, results_df.latitude), (results_df.longitude.name, results_df.longitude)])
+latitude = pd.Series(results_df.latitude).values
+longitude = pd.Series(results_df.longitude).values
+target = np.zeros(data_size)
+target=latitude
+#
+feature = np.zeros(shape=(data_size,8))
+feature[:,0]=date
+feature[:,1]=month
+feature[:,2]=year
+feature[:,3]=day
+feature[:,4]=hour
+feature[:,5]=minute
+feature[:,6]=second
+feature[:,7]=longitude
+#
+# print(feature.columns)
+# feature_array=pd.DataFrame.as_matrix(feature)
+# print(pd.DataFrame.as_matrix(feature))
+# target_array=pd.DataFrame.as_matrix(target)
+from sklearn.neighbors import KNeighborsClassifier as k
+knn=k(n_neighbors=1)
+#
+knn.fit(feature,target)
 
-feature = pd.DataFrame.from_items(
-    [(results_df.latitude.name, results_df.latitude), (results_df.longitude.name, results_df.longitude)])
-# print(target)
-print(type(day_series))
-print(len(day_series))
-print(day_series)
+# print(knn.predict())
