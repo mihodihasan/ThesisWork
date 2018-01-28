c9301da7b4defb5dc97ac3e54addd20d47cd2d91
mihodihasan <mihodihasan@gmail.com>
2018-01-28 14:13:00 +0600

    idk

--- main/__init__.py
+++ main/__init__.py
@@ -5,7 +5,7 @@ from sodapy import Socrata as repo
 # Example authenticated client (needed for non-public datasets):
 client = repo("data.cincinnati-oh.gov", "t1J7xReA9Slki6fndZ3ocYROj", "mihodihasan@gmail.com", "%&!Mhl0Mb%&!")
 
-results = client.get("w2ka-rfbi", limit=10)
+results = client.get("w2ka-rfbi", limit=80000)
 
 results_df = pd.DataFrame.from_records(results)
 data_size = len(results_df)
@@ -43,25 +43,37 @@ for i in results_df['time']:
 latitude = pd.Series(results_df.latitude).values
 longitude = pd.Series(results_df.longitude).values
 target = np.zeros(data_size)
-target=latitude
+target = latitude
 #
-feature = np.zeros(shape=(data_size,8))
-feature[:,0]=date
-feature[:,1]=month
-feature[:,2]=year
-feature[:,3]=day
-feature[:,4]=hour
-feature[:,5]=minute
-feature[:,6]=second
-feature[:,7]=longitude
+feature = np.zeros(shape=(data_size, 8))
+feature[:, 0] = date
+feature[:, 1] = month
+feature[:, 2] = year
+feature[:, 3] = day
+feature[:, 4] = hour
+feature[:, 5] = minute
+feature[:, 6] = second
+feature[:, 7] = longitude
 #
 # print(feature.columns)
 # feature_array=pd.DataFrame.as_matrix(feature)
 # print(pd.DataFrame.as_matrix(feature))
 # target_array=pd.DataFrame.as_matrix(target)
 from sklearn.neighbors import KNeighborsClassifier as k
-knn=k(n_neighbors=1)
-#
-knn.fit(feature,target)
 
-# print(knn.predict())
+knn = k(n_neighbors=2)
+#
+# knn.fit(feature, target)
+
+from sklearn import metrics
+
+# res = knn.predict(feature)
+# print(metrics.accuracy_score(target, res))
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(feature, target, test_size=0.1, random_state=0)
+knn.fit(X_train,y_train);
+y_pred=knn.predict(X_test)
+
+# print(metrics.accuracy_score(y_test, y_pred))
+print(results_df)
